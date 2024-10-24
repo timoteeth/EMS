c39b3d13760a2dfe47e5796312e558ab38af0a6d
timoteeth <tim.strawn1998@gmail.com>
2024-10-24 10:45:59 -0600

    Create Sensor.ino
    added sensor struct file

---
+++ src/Sensor.ino
@@ -0,0 +1,17 @@
+// Team Ecosense
+// Environmental Monitoring System
+// Last updated: 10/24/2024
+// 
+// Sensor Structure
+//
+// this code sets up the struct for a sensor and all associated variables
+
+struct Sensor{
+  int id;     // unique identifier for each sensor
+
+  int pinCount; // amount of pins being used to read from
+  int* pins;    // arduino pins being used by sensor
+  void (*readFunction)(int* pins, int pinCount);  // function for sensor to convert pin reading to real data
+  
+
+};
