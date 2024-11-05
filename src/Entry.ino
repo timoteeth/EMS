ba95f1d494f612b8fa52609e4c9a2dd5749f5cf1
timoteeth <tim.strawn1998@gmail.com>
2024-11-05 08:26:25 -0700

    Update Entry.ino

--- src/Entry.ino
+++ src/Entry.ino
@@ -1,3 +1,10 @@
+// Team Ecosense
+// Environmental Monitoring System
+// Last updated: 11/5/2024
+// 
+// Entry Structure
+//
+// this code sets up the struct for a data entry and all associated variables
 struct Entry{
 	char* data;
 	uint8_t size;
