ab1eca26b0d0255f8dca12fcc73531687aa8d3a0
timoteeth <tim.strawn1998@gmail.com>
2024-11-05 09:13:01 -0700

    Add files via upload

--- src/Sensor.ino
+++ src/Sensor.ino
@@ -1,20 +1,20 @@
 // Team Ecosense
 // Environmental Monitoring System
-// Last updated: 11/5/2024
+// Last updated: 10/24/2024
 // 
 // Sensor Structure
 //
 // this code sets up the struct for a sensor and all associated variables
 
 struct Sensor{
-  int id;     // unique identifier for each sensor
-  int pinCount; // amount of pins being used to read from
-  int* pins;    // arduino pins being used by sensor
-  struct Entry* buffer;
-  int buffi2c;
-  int buffCount;
-  int buffStride;
-  char* (format*)(struct Entry*);
+  int id;                 // unique identifier for each sensor
+  int pinCount;           // amount of pins being used to read from
+  int* pins;              // arduino pins being used by sensor
+  struct Entry* buffer;   // data from sensor to be stored in temperary buffer
+  int buffSize;           // allocated bytes for buffer
+  int buffCount;          // number of elements in buffer
+  int buffStride;         // byte length of each elemnet in buffer
+  char* (format*)(struct Entry*);   // stuff for SQL
 
   void (*readFunction)(int* pins, int pinCount);  // function for sensor to convert pin reading to real data
 };
